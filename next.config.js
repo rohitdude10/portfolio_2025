ba9637ce80b3e78d4d7ebe2522c51dffc3756fda
rohitdude10 <rohitkumardude10@gmail.com>
2025-03-09 17:24:37 +0530

    Simplify configuration and remove custom image loader

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio_2025' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio_2025' : '',
+  basePath: '/portfolio_2025',
+  assetPrefix: '/portfolio_2025/',
 };
 
 module.exports = nextConfig; 
